ee6554f979060f80b43d5e482fadba6d516d0e03
Chisheng Chen <johnny1001s000602@gmail.com>
2025-11-29 01:12:30 +0800

    Fix exploit error

--- baby_invalid_curve/exp.py
+++ baby_invalid_curve/exp.py
@@ -13,12 +13,13 @@ b = int(r.recvline().strip())
 r.recvuntil(b": ")
 p = int(r.recvline().strip())
 r.recvuntil(b": ")
-r.recvuntil(b"t(")
+r.recvuntil(b"(")
 G = tuple(map(int, r.recvline().strip(b")\n").decode().split(", ")))
 r.recvuntil(b": ")
-r.recvuntil(b"t(")
+r.recvuntil(b"(")
 y = tuple(map(int, r.recvline().strip(b")\n").decode().split(", ")))
-
+# print("yee")
+EE = EllipticCurve(GF(p), [a, b])
 r.recvuntil(b": ")
 ciphertext = bytes.fromhex(r.recvline().strip().decode())
 
@@ -26,47 +27,44 @@ proof.arithmetic(False)
 yees = []
 prods = 1
 done = False
-for b in range(1, 100):
+for bb in range(1, 100):
     if done:
         break
-    try:
-        if (4 * a**3 + 27 * b**2) % p == 0:
-            continue
-        E = EllipticCurve(GF(p), [a, b])
-        G = E.gens()[0]
-        ord = E.order()
-        for pp, e in factor(ord):
-            if pp**e < 2**40:
-                if gcd(prods, pp) != 1:
-                    continue
-                prods *= pp**e
-                if prods > 2**257:
-                    done = True
-                print(pp**e)
-                yees.append((G.xy(), b, pp**e, ord))
-    except:
-        pass
+    if (4 * a**3 + 27 * bb**2) % p == 0:
+        continue
+    E = EllipticCurve(GF(p), [a, bb])
+    GG = E.gen(0)
+    ord = GG.order()
+    for pp, e in factor(ord):
+        if pp**e < 2**40:
+            if gcd(prods, pp) != 1:
+                continue
+            prods *= pp**e
+            if prods > 2**257:
+                done = True
+            print(pp**e)
+            yees.append((GG.xy(), bb, pp**e, ord))
+
 with open("tmp.txt", "w") as f:
     f.write(str(yees))
 
 sols = []
 primes = []
 
-for G, b, pp, ord in yees:
-    r.sendlineafter(b": ", str(G[0]).encode())
-    r.sendlineafter(b": ", str(G[1]).encode())
+for GG, bb, pp, ord in yees:
+    r.sendlineafter(b": ", str(GG[0]).encode())
+    r.sendlineafter(b": ", str(GG[1]).encode())
     r.recvuntil(b"(")
     P = tuple(map(int, r.recvline().strip(b")\n").decode().split(", ")))
-    print(a, b)
-    try:
-        E = EllipticCurve(GF(p), [a, b])
-        G = E(G)
-        P = E(P)
-        sol = discrete_log((ord // pp) * P, (ord // pp) * G, ord=pp, operation="+")
-        sols.append(sol)
-        primes.append(pp)
-    except:
-        pass
+    print(bb, GG, pp)
+    E = EllipticCurve(GF(p), [a, bb])
+    GG = E(GG)
+    P = E(P)
+    x = bytes_to_long(b"b" * 16)
+    sol = discrete_log((ord // pp) * P, (ord // pp) * GG, ord=pp, operation="+")
+    sols.append(sol)
+    primes.append(pp)
+
 sol = crt(sols, primes)
 key = long_to_bytes(sol)
 assert(len(key) == 16)
